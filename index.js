106bf534818a325f89ee54aecbba487cedde16b7
Splittikin <45697042+Splittikin@users.noreply.github.com>
2023-03-24 09:31:27 -0400

    lunch implementation and organization

--- index.js
+++ index.js
@@ -24,14 +24,15 @@ const server = http.createServer((req, res) => {
         const request_segments = request.pathname.split('/')
         const request_arguments = request.query
 
-        if (request_segments[1] === '') {
+        if (request_segments[1] === '') { // If no path is specified
             res.writeHead(302, {
                 'Location': 'https://splittikin.github.io/FHS-News-Docs/'
             })
             res.end()
         } else if (request_segments[1] === 'favicon.ico') {
+            console.log('getting favicon')
             res.statusCode = 200
-            fs.createReadStream(path.resolve('./public/bruh.png'))
+            fs.createReadStream(path.resolve('./pages/bruh.png')).pipe(res)
         } else if (request_segments[1] === 'api') {
             if (request_segments[2] === 'article') {
                 getArticle(request_arguments, req, res)
@@ -45,12 +46,14 @@ const server = http.createServer((req, res) => {
                 search_date(request_arguments, res)
             } else if (request_segments[2] === 'weather') {
                 getWeather(res)
+            } else if (request_segments[2] === 'lunch') {
+                getLunch(res)
             } else {
                 returnError(400, res)
             }
         } else if (request_segments[1] === 'files') {
             res.statusCode = 200
-            fs.createReadStream(path.resolve('./public' + req.url.split('/files')[1])).pipe(res) // req.url.split('/files')[1] effectively trim '/files' from the start of the string
+            fs.createReadStream(path.resolve('./pages' + req.url.split('/files')[1])).pipe(res) // req.url.split('/files')[1] effectively trim '/files' from the start of the string
         } else {
             returnError(404, res)
         }
@@ -64,7 +67,7 @@ server.listen(port, hostname, () => {
 
 function resolveAttachments(item) {
     if (item.clubThumbnail != null) {
-        item.clubThumbnail = config.attachments_url + "clubs/" + item["clubId"] + "/" + item.clubThumbnail
+        item.clubThumbnail = config.attachments_url + "clubs/" + 0 + "/" + item.clubThumbnail
     } else if (item.articleThumbnail != null) {
         item.articleThumbnail = config.attachments_url + "articles/" + item["articleId"] + "/" + item.articleThumbnail
         item.topperIcon = config.attachments_url + "articles/" + item["articleId"] + "/" + item.topperIcon
@@ -74,17 +77,17 @@ function resolveAttachments(item) {
 
 async function returnError(errorCode, res) {
     res.statusCode = errorCode
-    fs.exists('./error_pages/' + errorCode + '.html', exists => {
+    fs.exists('./pages/error/' + errorCode + '.html', exists => {
         if (exists) {
-            fs.createReadStream('./error_pages/' + errorCode + '.html').pipe(res)
+            fs.createReadStream('./pages/error/' + errorCode + '.html').pipe(res)
         } else {
-            fs.createReadStream('./error_pages/generic.html').pipe(res)
+            fs.createReadStream('./pages/error/generic.html').pipe(res)
         }
     })
 }
 
 async function loadHome(arguments, res) {
-    let articlesNeeded = 3
+    let articlesNeeded = 5
     let articlesOffset = 0
     let includeExtras = true
     if (arguments["quantity"] != null) {
@@ -106,18 +109,32 @@ async function loadHome(arguments, res) {
         Weather
         Alert (Red/Silver)
         Alert
+        Lunch
         Articles
         Articles
         Articles
         Articles
         etc...
 
-        Weather and alerts are grabbed first so they appear at the top of the list.
+        Extras are grabbed in reverse order and added to the top of the list so they appear in order.
          */
 
         let returnData = []
         let extrasLength = 0
         if (includeExtras) {
+            // Get lunch and add it to the top of the list
+            let thisLunch = new Promise((resolve, reject) => {
+                fs.readFile('./extras/lunch.json', 'utf8', (err, data) => {
+                    if (err) {
+                        reject(err)
+                    } else {
+                        resolve(JSON.parse(data))
+                    }
+                })
+            })
+            extrasLength += 1
+            returnData.unshift(thisLunch)
+
             // Get alerts and add them to the top of the feed
             fs.readdirSync('./alerts/').sort().reverse().forEach(file => {
                 let thisAlert = new Promise((resolve, reject) => {
@@ -135,7 +152,7 @@ async function loadHome(arguments, res) {
 
             // Get weather and add it to the top of the feed
             let thisWeather = new Promise((resolve, reject) => {
-                fs.readFile('./weather/current.json', 'utf8', (err, data) => {
+                fs.readFile('./extras/weather.json', 'utf8', (err, data) => {
                     if (err) {
                         reject(err)
                     } else {
@@ -188,33 +205,32 @@ async function writeHome(returnArticles, res) { // Separated into a separate fun
 
 async function getWeather(res) {
     console.log("getting weather!!!")
+    fs.createReadStream('./extras/weather.json').pipe(res)
+}
 
-    let weatherReturn
-    fs.readFile('./weather/current.json', 'utf8', (err, data) => {
+async function getLunch(res) {
+    //  In the future, more than one lunch may be specified in lunch.json with a time range for each.
+    //  So, just directly reading lunch.json shouldn't be done as later on the server will have to figure
+    // out which lunch to return first.
+    fs.readFile('./extras/lunch.json', 'utf8', (err, data) => {
         if (err) {
             throw (err)
         } else {
-            weatherReturn = JSON.parse(data)
-            writeWeather(weatherReturn, res)
-        }
-    })
-}
-
-async function writeWeather(returnWeather, res) { // Separated into a separate function so that the server will wait until all the promises resolve before continuing
-    console.log(returnWeather)
-    const filePath = path.resolve('./generated/weather.json')
-    fs.writeFile(filePath, JSON.stringify(returnWeather, null, 2), function (err) {
-        if (err) {
-            throw err
-        } else {
-            res.statusCode = 200
-            fs.createReadStream(filePath).pipe(res)
+            let lunchReturn = JSON.parse(data)
+            fs.writeFile('./generated/lunch.json', JSON.stringify(lunchReturn, null, 2), (err) => {
+                if (err) {
+                    throw (err)
+                } else {
+                    res.statusCode = 200
+                    fs.createReadStream('./generated/lunch.json').pipe(res)
+                }
+            })
         }
     })
 }
 
 async function loadClubs(arguments, res) {
-    let clubsNeeded = 3
+    let clubsNeeded = 5
     let clubsOffset = 0
     if (arguments["quantity"] != null) {
         clubsNeeded = parseInt(arguments["quantity"], 10)
